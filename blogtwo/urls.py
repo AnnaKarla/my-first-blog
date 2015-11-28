bee9b01690bf6a28413c6c44e95cdd860a9ef620
anna <1253108@uni-wuppertal.de>
2015-11-28 15:42:55 +0100

    Added views to create/edit blog post inside the site.

--- blogtwo/urls.py
+++ blogtwo/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),s
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-
+    url(r'^post/new/$',
+        views.post_new,
+        name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$',
+        views.post_edit, name='post_edit'),
     ]
